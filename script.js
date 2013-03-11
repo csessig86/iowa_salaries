12ebd9416b0c48936b5dda35ad1e22c09b5e9e09
csessig86 <csessig86@gmail.com>
2013-03-11 18:20:38 -0500

    Delete unused function

--- script.js
+++ script.js
@@ -23,18 +23,6 @@ jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
 	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
 
-// Puts commas in number strings
-function numberFormat(nStr){
-	nStr += '';
-	x = nStr.split('.');
-	x1 = x[0];
-	x2 = x.length > 1 ? '.' + x[1] : '';
-	var rgx = /(\d+)(\d{3})/;
-	while (rgx.test(x1))
-	x1 = x1.replace(rgx, '$1' + ',' + '$2');
-	return x1 + x2;
-}
-
 // Where we'll put the data
 var newDataSet = [];
 
