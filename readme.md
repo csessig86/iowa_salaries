3eebfbae7e428edf140cca9068283a57fc02d285
csessig86 <csessig86@gmail.com>
2013-03-11 18:18:00 -0500

    Initial upload

---
+++ readme.md
@@ -0,0 +1,20 @@
+Turn DataSets into DataTables
+
+Uses:
+DataSets by The Miso Project:
+http://misoproject.com/dataset/
+- To work with a CSV and convert into Javascript arrays
+
+DataTables:
+http://www.datatables.net/
+- A Jquery plugin that allows us to work with the Javascript arrays
+- And turn them into a table
+
+Twitter's Bootstrap:
+http://twitter.github.com/bootstrap/
+- To style the table
+
+
+This example creates a database of Iowa public employee salaries
+- Data pulled from the Iowa Department of Adminstrative Services:
+https://www.legis.iowa.gov/LSAReports/salaryBook.aspx
---
+++ script.js
@@ -0,0 +1,100 @@
+// DataTables currency
+// Use to sort the table via currency
+jQuery.extend( jQuery.fn.dataTableExt.oSort, {
+	"currency-pre": function ( a ) {
+		a = (a==="-") ? 0 : a.replace( /[^\d\-\.]/g, "" );
+		return parseFloat( a );
+	},
+	
+	"currency-asc": function ( a, b ) {
+		return a - b;
+	},
+	
+	"currency-desc": function ( a, b ) {
+		return b - a;
+	}
+});
+
+// Define two custom functions (asc and desc) for string sorting
+jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+	return ((x < y) ? -1 : ((x > y) ?  0 : 0));
+};
+jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+};
+
+// Puts commas in number strings
+function numberFormat(nStr){
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1))
+	x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	return x1 + x2;
+}
+
+// Where we'll put the data
+var newDataSet = [];
+
+// Our column headers
+// Change these to fit your table
+var tableColumnSet =   [
+	{ "sTitle": "Employee" },
+	{ "sTitle": "Department" },
+	{ "sTitle": "Total FY12 Salary", "sType": "currency" },
+	{ "sTitle": "Gender", "sClass": "center" },
+	{ "sTitle": "County", "sClass": "center" },
+	{ "sTitle": "Position", "sClass": "center" }
+];
+
+// Our DataTable information
+// Don't need to change anything here
+// Unless you want to customize the table format
+function showInfo() {
+	$('#table_div').html( '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="table"></table>' );
+
+	// Push the data to the table
+	$('#table').dataTable( {
+		"bProcessing":true,
+		"sPaginationType": "bootstrap",
+		"iDisplayLength": 100,
+		"aaData": newDataSet,
+		"aoColumns": tableColumnSet,
+		// Pick which column we will sort by default
+		// For this table, we'll sort ascending by Total FY12 Salary
+		"aaSorting": [[ 2, "desc" ]],
+		"oLanguage": {
+			"sLengthMenu": "_MENU_ records per page"
+		}
+	});
+}
+
+// Load up the CSV using Miso.Dataset
+$(document).ready( function() {
+	// Change URL to the right path for your CSV
+	var ds = new Miso.Dataset({
+  		url : 'csvs/salary_book_clean.csv',
+  		delimiter : ','
+  	});
+
+	// Run this after we load our CSV
+	ds.fetch({ success : function() {
+    	this.each(function(row, rowIndex) {
+    		// Change these variables to match your column names
+    		var employeeData = row.Employee;
+    		var departmentData = row.Department;
+    		var salaryData = row.TotalFY12Salary;
+    		var genderData = row.Gender;
+    		var countyData = row.County;
+    		var positionData = row.Position;
+    		// Put information in an array and push it to our table
+    		// Change these variables to match variables above
+    		var myArray = [employeeData, departmentData, salaryData, genderData, countyData, positionData];
+			newDataSet.push(myArray);
+  		});
+    	// Call DataTable function showInfo
+		showInfo();
+	}});
+});
